3ca02ccb10fa13a85a62b218fc27dd1c9f967913
Darrell-Rafa <drafa0609@gmail.com>
2023-12-22 23:27:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// header
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  header.style.backgroundColor =
+    window.pageYOffset > 16 ? "#FFF" : "transparent";
+  header.style.padding = window.pageYOffset > 16 ? "1rem 2rem" : "2rem";
+  header.style.boxShadow =
+    window.pageYOffset > 16
+      ? "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
+      : "0 0 #0000";
+});
+
+// navbar
+
+const openNavbar = document.getElementById("open-navbar");
+const closeNavbar = document.getElementById("close-navbar");
+const navbarSection = document.getElementById("mobile-navbar");
+
+openNavbar.addEventListener("click", () => {
+  document.body.style.overflowY = "hidden";
+  navbarSection.classList.remove("left-[150%]");
+  navbarSection.classList.add("left-0");
+});
+
+closeNavbar.addEventListener("click", () => {
+  document.body.style.overflowY = "auto";
+  navbarSection.classList.remove("left-0");
+  navbarSection.classList.add("left-[150%]");
+});
